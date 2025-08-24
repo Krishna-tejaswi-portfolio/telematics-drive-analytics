b3236986c04ec304484508f3b014070c175ef549
Krishna-tejaswi-portfolio <tkrishnatejaswi@gmail.com>
2025-08-24 23:26:34 +0530

    Delete serving/superset_init.sh

--- serving/superset_init.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-superset fab create-admin \
-  --username admin --firstname a --lastname b --email admin@example.com --password admin
-superset db upgrade
-superset init
